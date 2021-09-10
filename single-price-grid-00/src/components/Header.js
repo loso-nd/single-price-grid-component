1993740a3647d5e325dce1b0a6f1de3bc037d6b9
Nahum <losond8@gmail.com>
2021-09-09 18:50:00 -0700

    Import semantic UI, Updated Styling, Completed.

--- single-price-grid-00/src/components/Header.js
+++ single-price-grid-00/src/components/Header.js
@@ -2,23 +2,19 @@ import React from 'react'
 
 function Header() {
     return (
-        <>
         <div className="header">
             <h1>Join our Community</h1>
-
+            <h2>30-day, hassle-free money back guarantee</h2>
 
 
             <div className="content">
-                <h2>30-day, hassle-free money back guarantee</h2>
-                <p className="reviews">Gain access to our full library of tutorials along with expert code reviews.
-                </p>
-
-                <p>Perfect for any developer who are serious about honing their.</p>
+                
+                <p>Gain access to our full library of tutorials along with expert code reviews.
+                <br/>
+                Perfect for any developer who are serious about honing their skills.</p>
             </div>
 
-
         </div>
-        </>
     )
 }
 
--- single-price-grid-00/src/components/Subscription.js
+++ single-price-grid-00/src/components/Subscription.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react';
+import { List } from 'semantic-ui-react';
 
 function Subscription() {
     return (
         <div className="sub-container">
             <div className="subscription-details">
-                <h1> Montly Subscription</h1>
+                <h3> Montly Subscription</h3>
+                <div className="left">
+                    <p className="price">
+                    $29 <span>per month</span> 
+                    <br/>
+                        <span className="access">Full access for less than $1 a day</span>
+                    </p>
+                    <Button color='olive' size='massive'fluid style={{width: '80%', borderRadius: '8px', fontsize: '30px'}}> Sign Up</Button>
+
+
+                </div>
             </div>
             <div className="why-us">
-                <h1> Why Us</h1>
+                <h3> Why Us</h3>
+                <List>
+                    <List.Item>Tutorials by industry experts</List.Item>
+                    <List.Item>Peer & expert code reveiw</List.Item>
+                    <List.Item>Codding exercises</List.Item>
+                    <List.Item>Access to our GitHub repos</List.Item>
+                    <List.Item>Community forum</List.Item>
+                    <List.Item>Coding exercises</List.Item>
+                </List>
             </div>
             
         </div>
