a9e9743d23b02e5ac6cc5216ad4b4e7737bad36c
Nahum <losond8@gmail.com>
2021-09-12 08:18:58 -0700

    Updated CSS.

--- client/single-price-grid-00/src/components/Header.js
+++ client/single-price-grid-00/src/components/Header.js
@@ -3,19 +3,17 @@ import React from 'react'
 function Header() {
     return (
         <div className="header">
-            <h1>Join our Community</h1>
-            <h2>30-day, hassle-free money back guarantee</h2>
+            <div  className="content">
+                <h1>Join our Community</h1>
+                <h2>30-day, hassle-free money back guarantee</h2>
 
-
-            <div className="content">
-                
-                <p>Gain access to our full library of tutorials along with expert code reviews.
-                <br/>
-                Perfect for any developer who are serious about honing their skills.</p>
+                <div>
+                    <p>Gain access to our full library of tutorials along with expert code reviews.
+                    <br/>
+                    Perfect for any developer who are serious about honing their skills.</p>
+                </div>
             </div>
-
         </div>
     )
 }
-
 export default Header
--- client/single-price-grid-00/src/components/Subscription.js
+++ client/single-price-grid-00/src/components/Subscription.js
@@ -5,30 +5,37 @@ import { List } from 'semantic-ui-react';
 function Subscription() {
     return (
         <div className="sub-container">
-            <div className="subscription-details">
-                <h3> Montly Subscription</h3>
-                <div className="left">
-                    <p className="price">
-                    $29 <span>per month</span> 
-                    <br/>
-                        <span className="access">Full access for less than $1 a day</span>
-                    </p>
-                    <Button color='olive' size='massive' style={{minWidth: '80%', maxWidth: '80%', borderRadius: '8px', fontsize: '30px'}}> Sign Up</Button>
+            <div className="sub-details">
+                <div className="subscription-details"> 
+                    <h3> Montly Subscription</h3>
+                    <div>
+                        <p className="price">
+                            <span> $29</span> per month
+                            <br/>
+                            {/* <span className="access">Full access for less than $1 a day</span> */}
+                        </p>
+                        <p className="access">
+                            Full access for less than $1 a day
+                        </p>
+                        <Button color='olive' size='massive' style={{minWidth: '90%', maxWidth: '90%', borderRadius: '8px', fontsize: '30px'}}> Sign Up</Button>
+                    </div>
                 </div>
+            
+                <div className="why-us"> 
+                <h3> Why Us</h3>
+                    <div className="list">
+                        <List>
+                            <List.Item>Tutorials by industry experts</List.Item>
+                            <List.Item>Peer & expert code reveiw</List.Item>
+                            <List.Item>Codding exercises</List.Item>
+                            <List.Item>Access to our GitHub repos</List.Item>
+                            <List.Item>Community forum</List.Item>
+                            <List.Item>Coding exercises</List.Item>
+                        </List>
+                    </div>
+                </div>         
             </div>
 
-            <div className="why-us">
-                <h3> Why Us</h3>
-                <List>
-                    <List.Item>Tutorials by industry experts</List.Item>
-                    <List.Item>Peer & expert code reveiw</List.Item>
-                    <List.Item>Codding exercises</List.Item>
-                    <List.Item>Access to our GitHub repos</List.Item>
-                    <List.Item>Community forum</List.Item>
-                    <List.Item>Coding exercises</List.Item>
-                </List>
-            </div>
-            
         </div>
     )
 }
